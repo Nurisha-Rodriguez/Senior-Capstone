c285f9f35a15a874a78c882de22c8e3ed9516c7c
Nurisha-Rodriguez <rodrigun@union.edu>
2016-02-11 17:17:58 -0500

    Thesis

---
+++ Capstone/Capstone.ino
@@ -0,0 +1,39 @@
+#include <call.h>
+#include <gps.h>
+#include <GSM.h>
+#include <HWSerial.h>
+#include <inetGSM.h>
+#include <LOG.h>
+#include <SIM900.h>
+#include <sms.h>
+#include <Streaming.h>
+#include <WideTextFinder.h>
+#include "Adafruit_FONA.h"
+
+// standard pins for the shield, adjust as necessary
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+
+// We default to using software serial. If you want to use hardware serial
+// (because softserial isnt supported) comment out the following three lines 
+// and uncomment the HardwareSerial line
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+//SoftwareSerial *fonaSerial = &fonaSS;
+
+// Hardware serial is also possible!
+//  HardwareSerial *fonaSerial = &Serial1;
+
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
